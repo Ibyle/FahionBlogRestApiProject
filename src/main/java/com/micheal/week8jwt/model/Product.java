c756ec77cbdd236f49e6850c76d58b4b74c19f13
Ibyle <veektorscreation@gmail.com>
2024-01-12 16:40:23 +0100

    Fashion Bloh
    Swag Center

---
+++ src/main/java/com/micheal/week8jwt/model/Product.java
@@ -0,0 +1,28 @@
+package com.micheal.week8jwt.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.util.function.Function;
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue
+
+    private String id;
+    private String name;
+    private double modelNo;
+    private String category;
+
+    public Product (String name, double modelNo, String category){
+        this.name = name;
+        this.modelNo = modelNo;
+        this.category = category;
+    }
+
+
+    public Product() {
+
+    }
+}
